f7b90f0c9ddd6b0415de6b71041bf2bff7fe73b2
fbonhomme <franck.bonhomme@gmail.com>
2018-10-18 18:43:36 +0200

    ajout 3 chants complet

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
-window.sr = ScrollReveal({ reset: true });
-sr.reveal('aside', { duration: 500 });
-sr.reveal('article', { duration: 500 });
+
+$("#btn1").on('click', (e) =>{
+    e.preventDefault()
+    $.get("./parole.html",(data) =>{
+        const sortie = $("#sortie")
+        sortie.html(data);
+        })
+})
+
+
+$("#btn2").on('click', (e) =>{
+    e.preventDefault()
+    $.get("./NosYeusSontSurToi.html",(data) =>{
+        const sortie = $("#sortie")
+        sortie.html(data);
+        })
+ })
+
+$("#btn3").on('click', (e) =>{
+    e.preventDefault()
+    $.get("./CeNomSiMerveilleux.html",(data) =>{
+        const sortie = $("#sortie")
+        sortie.html(data);
+        })  
+    })   
+  
+
+
